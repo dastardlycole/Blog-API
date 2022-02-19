8e88c23317017b6ddbfa1666dded68db9d707576
dastardlycole <ifemide@gmail.com>
2022-02-19 02:30:27 +0100

    url change

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('users/reset_password', views.reset_password, name="reset password"),
     path('auth/', views.login_view, name="login"),
     path('auth/forgot_password', views.forgot_password, name="forgot password"),
-    path('auth/verify_otp_password', views.verify_otp_pass, name="verify otp change pass"),
+    path('auth/verify_otp_forgot_password', views.verify_otp_pass, name="verify otp change pass"),
     path('users/profile/', views.profile_view, name="profile"),
     path("users/verify_otp/", views.verify_otp),
     path("users/new_otp/", views.new_otp),
