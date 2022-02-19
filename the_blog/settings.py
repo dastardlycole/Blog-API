209f8d5ad8e5ec60145ead4e7fef4c2cda2ea29b
dastardlycole <ifemide@gmail.com>
2022-02-19 02:16:02 +0100

    allowed hosts

--- the_blog/settings.py
+++ the_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ifemide-cole-blog-api.herokuapp.com']
+ALLOWED_HOSTS = ['ifemide-cole-blog-api.herokuapp.com']
 
 
 # Application definition
